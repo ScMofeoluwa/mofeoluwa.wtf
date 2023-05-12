1a68d82fad506c6ff0ccf19e3008374da1d1a0ac
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2023-05-12 15:46:59 +0100

    updated og:image and site title

--- src/site.config.ts
+++ src/site.config.ts
@@ -16,12 +16,12 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Mofeoluwa Lijadu",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Ikigai",
+	title: "mofeoluwa.wtf",
 	// Meta property used as a default description meta property
 	description: "Blog site for Mofeoluwa",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
-	// Meta property, found in src/components/BaseHead.astro L:42
+	// Meta property, found in srxc/components/BaseHead.astro L:42
 	ogLocale: "en_GB",
 	// Sets the meta data theme-color, found in src/components/BaseHead.astro L:34. Toggling the dark mode will update the meta content with either light/dark color, implementation in src/layouts/Base.astro L:41.
 	themeColorLight: "#fafafa",
